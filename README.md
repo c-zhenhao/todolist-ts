f033780238e9dbeb6fef77d94f82e149bf08ecd0
czhenhao <czhenhao@hotmail.com>
2022-06-11 00:09:56 +0800

    created todo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+https://typeofnan.dev/your-first-react-typescript-project-todo-app/
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import React from "react";
-import Hello from "./components/hello";
+import React, { useState } from "react";
+// import { arrayBuffer } from "stream/consumers";
+// import Hello from "./components/hello"
 // import logo from './logo.svg';
 // import './App.css';
 
+import { TodoListItem } from "./components/TodoListItem";
+
+const initialTodos: Todo[] = [
+  {
+    text: "walk the dog",
+    complete: false,
+  },
+  {
+    text: "write app",
+    complete: true,
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <Hello />
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
     </div>
   );
 }
